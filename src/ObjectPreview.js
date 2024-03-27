35b43c62c3bc9c4daa4a8b56891590890d245c21
loicraza <48524193+loicraza@users.noreply.github.com>
2024-03-27 13:30:32 +0100

    Double scrollbar fixed,  object list images are clickable and CV backdrop added

--- src/ObjectPreview.js
+++ src/ObjectPreview.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ObjectPreview.css";
+import Backdrop from "@mui/material/Backdrop";
 
 function ObjectPreview({ found, objectTitle }) {
+  const [openBackdrop, setOpenBackdrop] = useState(false);
   const item = found.find((item) => item.title === objectTitle);
+
+  const handleClose = () => {
+    setOpenBackdrop(false);
+  };
+
+  const handleOpen = () => {
+    setOpenBackdrop(true);
+  };
+
   return (
     <div className="ObjectPreview">
       <img src={item ? item.img : "waiting"} width={"50%"} alt={objectTitle} />
       <p>Bravo ! Vous avez trouvé un objet !</p>
-      <button className="Content">Voir de plus près</button>
+      <button className="Content" onClick={handleOpen}>Examiner de plus près</button>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={openBackdrop}
+        onClick={handleClose}
+      >
+        <div className="Help">
+          <p>oui</p>
+        </div>
+      </Backdrop>
     </div>
   );
 }
--- src/Objects.js
+++ src/Objects.js
@@ -1,22 +1,41 @@
 import React, { useEffect, useState } from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import "./Objects.css";
 
-function Objects({ found, unfound}) {
+function Objects({ found, unfound }) {
+
+  const handleClick = () => {
+    console.log("click");
+    
+  };
 
   return (
-    <div>
-      <ImageList sx={{ width: 300, height: 1000 }} cols={2} rowHeight={164}>
+    <div className="object-list">
+      <h2>Objets à trouver</h2>
+      <ImageList
+        sx={{
+          width: 300,
+          height: 1000,
+          overflow: "hidden", 
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        }}
+        cols={2}
+        rowHeight={164}
+      >
         {found.map((item) => (
           <ImageListItem key={item.img}>
             <img
               srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
               src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+              className="object-image"
               alt={item.title}
+              onClick={handleClick}
             />
           </ImageListItem>
         ))}
-
         {unfound.map((item) => (
           <ImageListItem key={item.img}>
             <img

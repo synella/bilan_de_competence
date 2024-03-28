96a1d027d786c6ce4a6a1075bd9b84354dd0b466
loicraza <48524193+loicraza@users.noreply.github.com>
2024-03-28 11:27:01 +0100

    Hidden objects after found

--- src/Finish.js
+++ src/Finish.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@mui/material";
+
 
 function Finish() {
   return (
-    <div>Finish</div>
-  )
+    <div>
+      <div className="Home">
+        <div className="Popup">
+          <div className="Bob">
+            <img src="Bob.png" width="30%" alt="Bob" />
+          </div>
+          <div className="Hello">
+            <p>Bonjour ! Vous tombez bien</p>
+          </div>
+          <div className="Text">
+            {/* <Button className="Button" onClick={handleClick}>
+              C'est parti
+            </Button> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Finish
+export default Finish;
--- src/Scene.js
+++ src/Scene.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Scene.css";
 import Finish from "./Finish";
+import Home from "./Home";
 import Drawer from "@mui/material/Drawer";
 import Objects from "./Objects";
 import Backdrop from "@mui/material/Backdrop";
@@ -21,92 +22,92 @@ function Scene() {
     {
       img: "./objects/books.png",
       title: "books",
-      candidat: "./candidats/achille_attentes.png"
+      candidat: "./candidats/achille_attentes.png",
     },
     {
       img: "./objects/books2.png",
       title: "books2",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/cup.png",
       title: "cup",
-      candidat: "./candidats/achille_centre.png"
+      candidat: "./candidats/achille_centre.png",
     },
     {
       img: "./objects/files.png",
       title: "files",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/glasses.png",
       title: "glasses",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/penBox.png",
       title: "penBox",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/notebook.png",
       title: "notebook",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/notebook2.png",
       title: "notebook2",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/notebook3.png",
       title: "notebook3",
-      candidat: "./candidats/achille_itineraire.png"
+      candidat: "./candidats/achille_itineraire.png",
     },
     {
       img: "./objects/pencils.png",
       title: "pencils",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/plant.png",
       title: "plant",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/mug.png",
       title: "mug",
-      candidat: "./candidats/achille_traits.png"
+      candidat: "./candidats/achille_traits.png",
     },
     {
       img: "./objects/rug.png",
       title: "rug",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/sheets.png",
       title: "sheets",
-      candidat: "./candidats/achille_compe.png"
+      candidat: "./candidats/achille_compe.png",
     },
     {
       img: "./objects/sheets2.png",
       title: "sheets2",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/telephone.png",
       title: "telephone",
-      candidat: "./candidats/achille_cv.png"
+      candidat: "./candidats/achille_cv.png",
     },
     {
       img: "./objects/vase.png",
       title: "vase",
-      candidat: ""
+      candidat: "",
     },
     {
       img: "./objects/wallet.png",
       title: "wallet",
-      candidat: ""
+      candidat: "",
     },
   ]);
   const [found, setFound] = useState([]);
@@ -132,6 +133,9 @@ function Scene() {
 
   const handleClose = () => {
     setOpenBackdropFound(false);
+    if (unfound.length === 0) {
+      navigate("/Home");
+    }
   };
 
   const toggleDrawer = (newOpen) => () => {
@@ -158,188 +162,270 @@ function Scene() {
 
   return (
     <div className="Scene">
-
-      {unfound.length === 0 ? (
-        <Finish />
-      ) : (
-        <>
-          {/* <button className="cup" onClick={() => handleClick("cup")} style={{ display: found.some(item => item.title === "cup") ? "none" : "block" }}>
-        cup
-      </button> */}
-          <img
-            src="./objects/cup.png"
-            className="cup"
-            onClick={() => handleClick("cup")}
-            alt="cup"
-            draggable="false"
-          />
-          <img
-            src="./objects/telephone.png"
-            className="telephone"
-            onClick={() => handleClick("telephone")}
-            alt="telephone"
-            draggable="false"
-          />
-          <img
-            src="./objects/vase.png"
-            className="vase"
-            onClick={() => handleClick("vase")}
-            alt="vase"
-            draggable="false"
-          />
-          <img
-            src="./objects/wallet.png"
-            className="wallet"
-            onClick={() => handleClick("wallet")}
-            alt="wallet"
-            draggable="false"
-          />
-          <img
-            src="./objects/books.png"
-            className="books"
-            onClick={() => handleClick("books")}
-            alt="books"
-            draggable="false"
-          />
-          <img
-            src="./objects/books2.png"
-            className="books2"
-            onClick={() => handleClick("books2")}
-            alt="books2"
-            draggable="false"
-          />
-          <img
-            src="./objects/files.png"
-            className="files"
-            onClick={() => handleClick("files")}
-            alt="files"
-            draggable="false"
-          />
-          <img
-            src="./objects/glasses.png"
-            className="glasses"
-            onClick={() => handleClick("glasses")}
-            alt="glasses"
-            draggable="false"
-          />
-          <img
-            src="./objects/notebook.png"
-            className="notebook"
-            onClick={() => handleClick("notebook")}
-            alt="notebook"
-            draggable="false"
-          />
-          <img
-            src="./objects/notebook2.png"
-            className="notebook2"
-            onClick={() => handleClick("notebook2")}
-            alt="notebook2"
-            draggable="false"
-          />
-          <img
-            src="./objects/notebook3.png"
-            className="notebook3"
-            onClick={() => handleClick("notebook3")}
-            alt="notebook3"
-            draggable="false"
-          />
-          <img
-            src="./objects/penBox.png"
-            className="penBox"
-            onClick={() => handleClick("penBox")}
-            alt="penBox"
-            draggable="false"
-          />
-          <img
-            src="./objects/pencils.png"
-            className="pencils"
-            onClick={() => handleClick("pencils")}
-            alt="pencils"
-            draggable="false"
-          />
-          <img
-            src="./objects/plant.png"
-            className="plant"
-            onClick={() => handleClick("plant")}
-            alt="plant"
-            draggable="false"
-          />
-          <img
-            src="./objects/rug.png"
-            className="rug"
-            onClick={() => handleClick("rug")}
-            alt="rug"
-            draggable="false"
-          />
-          <img
-            src="./objects/sheets.png"
-            className="sheets"
-            onClick={() => handleClick("sheets")}
-            alt="sheets"
-            draggable="false"
-          />
-          <img
-            src="./objects/sheets2.png"
-            className="sheets2"
-            onClick={() => handleClick("sheets2")}
-            alt="sheets2"
-            draggable="false"
-          />
-          <img
-            src="./objects/mug.png"
-            className="mug"
-            onClick={() => handleClick("mug")}
-            alt="mug"
-            draggable="false"
-          />
-          <Backdrop
-            sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-            open={openBackdropFound}
-          >
-            <ObjectPreview objectTitle={objectTitle} found={found} />
-            <CloseIcon className="close-icon" onClick={handleClose} />
-          </Backdrop>
-          <Badge
-            badgeContent={badgeContentValue}
-            sx={{
-              ".MuiBadge-badge": {
-                backgroundColor: "#f44336",
-                color: "#ffffff",
-              },
-            }}
-            className="bob-badge"
-          >
-            <ClickAwayListener onClickAway={handleCloseTooltip}>
-              <Tooltip
-                open={openTooltip}
-                title={
-                  <span style={{ fontSize: "20px" }}>
-                    <p>
-                      Je ne sais pas comment vous remercier ! N'hésitez pas à
-                      revenir vers moi pour visualiser les objets à trouver ainsi
-                      que leurs contenus. Bonne chance !
-                    </p>
-                  </span>
-                }
-                placement="right"
-                arrow
-              >
-                <img
-                  src="Bob_thoughful.png"
-                  className="Bob_thoughtful"
-                  width="10%"
-                  alt="arrow"
-                  onClick={toggleDrawer(true)}
-                  draggable="false"
-                />
-              </Tooltip>
-            </ClickAwayListener>
-          </Badge>
-          <Drawer open={open} onClose={toggleDrawer(false)}>
-            <Objects found={found} unfound={unfound} />
-          </Drawer>
-        </>
-      )}
+      <>
+        <img
+          src="./objects/cup.png"
+          className="cup"
+          onClick={() => handleClick("cup")}
+          alt="cup"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "cup")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/telephone.png"
+          className="telephone"
+          onClick={() => handleClick("telephone")}
+          alt="telephone"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "telephone")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/vase.png"
+          className="vase"
+          onClick={() => handleClick("vase")}
+          alt="vase"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "vase")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/wallet.png"
+          className="wallet"
+          onClick={() => handleClick("wallet")}
+          alt="wallet"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "wallet")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/books.png"
+          className="books"
+          onClick={() => handleClick("books")}
+          alt="books"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "books")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/books2.png"
+          className="books2"
+          onClick={() => handleClick("books2")}
+          alt="books2"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "books2")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/files.png"
+          className="files"
+          onClick={() => handleClick("files")}
+          alt="files"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "files")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/glasses.png"
+          className="glasses"
+          onClick={() => handleClick("glasses")}
+          alt="glasses"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "glasses")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/notebook.png"
+          className="notebook"
+          onClick={() => handleClick("notebook")}
+          alt="notebook"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "notebook")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/notebook2.png"
+          className="notebook2"
+          onClick={() => handleClick("notebook2")}
+          alt="notebook2"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "notebook2")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/notebook3.png"
+          className="notebook3"
+          onClick={() => handleClick("notebook3")}
+          alt="notebook3"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "notebook3")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/penBox.png"
+          className="penBox"
+          onClick={() => handleClick("penBox")}
+          alt="penBox"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "penBox")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/pencils.png"
+          className="pencils"
+          onClick={() => handleClick("pencils")}
+          alt="pencils"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "pencils")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/plant.png"
+          className="plant"
+          onClick={() => handleClick("plant")}
+          alt="plant"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "plant")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/rug.png"
+          className="rug"
+          onClick={() => handleClick("rug")}
+          alt="rug"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "rug")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/sheets.png"
+          className="sheets"
+          onClick={() => handleClick("sheets")}
+          alt="sheets"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "sheets")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/sheets2.png"
+          className="sheets2"
+          onClick={() => handleClick("sheets2")}
+          alt="sheets2"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "sheets2")
+              ? "none"
+              : "block",
+          }}
+        />
+        <img
+          src="./objects/mug.png"
+          className="mug"
+          onClick={() => handleClick("mug")}
+          alt="mug"
+          draggable="false"
+          style={{
+            display: found.some((item) => item.title === "mug")
+              ? "none"
+              : "block",
+          }}
+        />
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={openBackdropFound}
+        >
+          <ObjectPreview objectTitle={objectTitle} found={found} />
+          <CloseIcon className="close-icon" onClick={handleClose} />
+        </Backdrop>
+        <Badge
+          badgeContent={badgeContentValue}
+          sx={{
+            ".MuiBadge-badge": {
+              backgroundColor: "#f44336",
+              color: "#ffffff",
+            },
+          }}
+          className="bob-badge"
+        >
+          <ClickAwayListener onClickAway={handleCloseTooltip}>
+            <Tooltip
+              open={openTooltip}
+              title={
+                <span style={{ fontSize: "20px" }}>
+                  <p>
+                    Je ne sais pas comment vous remercier ! N'hésitez pas à
+                    revenir vers moi pour visualiser les objets à trouver ainsi
+                    que leurs contenus. Bonne chance !
+                  </p>
+                </span>
+              }
+              placement="right"
+              arrow
+            >
+              <img
+                src="Bob_thoughful.png"
+                className="Bob_thoughtful"
+                width="10%"
+                alt="arrow"
+                onClick={toggleDrawer(true)}
+                draggable="false"
+              />
+            </Tooltip>
+          </ClickAwayListener>
+        </Badge>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          <Objects found={found} unfound={unfound} />
+        </Drawer>
+      </>
     </div>
   );
 }
